a20dd239b16a5fa4d5b59c20a31535136d5d30e3
German de la Cruz <german.delacruz@gmail.com>
2020-11-04 18:58:44 -0300

    Version 1 Quick and Dirty

--- src/main/java/com/emorgan/components/sevenDisplay/SevenDisplay.java
+++ src/main/java/com/emorgan/components/sevenDisplay/SevenDisplay.java
@@ -4,11 +4,28 @@ import com.emorgan.components.sevenDisplay.model.Result;
 
 public class SevenDisplay {
 
-    public SevenDisplay(){
+    String[] dict1 = new String[]{" _ ", " ", " _ ", "_ ", "   ", " _ ", " _ ", "_ ", " _ ", " _ "};
+    String[] dict2 = new String[]{"| |", "|", " _|", "_|", "|_|", "|_ ", "|_ ", " |", "|_|", "|_|"};
+    String[] dict3 = new String[]{"|_|", "|", "|_ ", "_|", "  |", " _|", "|_|", " |", "|_|", " _|"};
+
+    public SevenDisplay() {
 
     }
 
-    public Result construct (Integer number){
-        return new Result();
+    public Result construct(Integer number) {
+        assert number >= 0 : "Number must be positive";
+
+        StringBuilder line1 = new StringBuilder();
+        StringBuilder line2 = new StringBuilder();
+        StringBuilder line3 = new StringBuilder();
+
+        for (Integer digit : Integer.toString(number).chars().map(c -> c - '0').toArray()) {
+            line1.append(dict1[digit]).append(" ");
+            line2.append(dict2[digit]).append(" ");
+            line3.append(dict3[digit]).append(" ");
+        }
+
+        return new Result(line1.toString(), line2.toString(), line3.toString());
+
     }
 }
--- src/main/java/com/emorgan/components/sevenDisplay/model/Result.java
+++ src/main/java/com/emorgan/components/sevenDisplay/model/Result.java
@@ -2,17 +2,31 @@ package com.emorgan.components.sevenDisplay.model;
 
 public class Result {
 
-    public String getLine1(){
-        return null;
+    private final String line1;
+    private final String line2;
+    private final String line3;
+
+    public Result(String line1, String line2, String line3) {
+        this.line1 = line1;
+        this.line2 = line2;
+        this.line3 = line3;
     }
-    public String getLine2(){
-        return null;
+
+    public String getLine1() {
+        return line1;
     }
-    public String getLine3(){
-        return null;
+
+    public String getLine2() {
+        return line2;
     }
-    public String toString(){
-        return null;
+
+    public String getLine3() {
+        return line3;
+    }
+
+    public String toString() {
+
+        return line1 + "\n" + line2 + "\n" + line3;
     }
 
 }
